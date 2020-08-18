e7278405771b39c108f8d43bfca125e2a7e6db7a
Leonardo Freitas <leonardofreitasdev@gmail.com>
2020-08-18 19:49:22 -0300

    crud de categoria

--- categories/urls.py
+++ categories/urls.py
@@ -7,4 +7,6 @@ app_name = 'categories'
 urlpatterns = [
     path('', views.CategoryList.as_view(), name='list'),
     path('create/', views.CategoryCreate.as_view(), name='create'),
+    path('update/<int:pk>/', views.CategoryUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', views.CategoryDelete.as_view(), name='delete'),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Category
@@ -8,8 +9,30 @@ from .models import Category
 class CategoryList(LoginRequiredMixin, ListView):
     model = Category
 
+    def get_queryset(self):
+        queryset = Category.objects.all()
+        search = self.request.GET.get('search', False)
+        if search:
+            queryset = queryset.filter(name__contains=search)
+        return queryset
+
 
 class CategoryCreate(LoginRequiredMixin, CreateView):
     model = Category
     fields = ['name']
     success_url = reverse_lazy('categories:list')
+
+
+class CategoryUpdate(LoginRequiredMixin, UpdateView):
+    model = Category
+    fields = ['name']
+    success_url = reverse_lazy('categories:list')
+
+
+class CategoryDelete(LoginRequiredMixin, View):
+    
+    def get(self, request, pk):
+        category = get_object_or_404(Category, pk=pk)
+        category.delete()
+        return redirect(reverse_lazy('categories:list'))
+
